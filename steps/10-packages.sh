76940faf12b88095f4c3959b17d960c64b23efc8
Phill Hood <phill@phillhood.ca>
2026-07-05 09:01:30 -0400

    add package lists and package-install step

---
+++ steps/10-packages.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Step: install packages. Consumes lib/distro.sh (ensure_aur_helper, pkg_install)
+# and packages/*.txt. Honors WITH_K8S. Requires BOOTSTRAP_DIR to be set.
+
+# Print package names from the given files, stripping #-comments and blank lines.
+_read_packages() {
+  local f line
+  for f in "$@"; do
+    [ -r "$f" ] || die "package file not found: $f"
+    while IFS= read -r line || [ -n "$line" ]; do
+      line="${line%%#*}"                 # drop comments
+      line="${line//[[:space:]]/}"       # drop all whitespace
+      [ -n "$line" ] && printf '%s\n' "$line"
+    done < "$f"
+  done
+}
+
+step_packages() {
+  ensure_aur_helper
+  local dir="$BOOTSTRAP_DIR/packages"
+  local files=("$dir/core.txt" "$dir/cli.txt" "$dir/docker.txt")
+  [ "${WITH_K8S:-0}" = 1 ] && files+=("$dir/k8s.txt")
+  local pkgs; mapfile -t pkgs < <(_read_packages "${files[@]}")
+  info "installing ${#pkgs[@]} packages via the package manager"
+  pkg_install "${pkgs[@]}"
+}
